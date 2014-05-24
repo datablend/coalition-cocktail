0bba3c6107c4791e3683796de7e3fc415b61ac22
Quentin Mullens <q@datablend.be>
2014-05-24 13:14:46 +0200

    Added all topics

--- tweet-fetcher/Configuration.php
+++ tweet-fetcher/Configuration.php
@@ -11,6 +11,86 @@ class Configuration {
     );
 
     static $topicIds = array(
+
+        
+// PROJECT Candidates - European - keyword
+        26701 => '', // European Candidates CD&V Keyword Search
+        26724 => '', // European Candidates CDH Keyword Search
+        26737 => '', // European Candidates Debout Les Belges Keyword Search
+        26704 => '', // European Candidates Ecolo Keyword Search
+        26727 => '', // European Candidates FDF Keyword Search
+        26706 => '', // European Candidates Groen Keyword Search
+        26760 => '', // European Candidates MR Keyword Search
+        26709 => '', // European Candidates Open VLD Keyword Search
+        26744 => '', // European Candidates PS Keyword Search
+        26723 => '', // European Candidates PTB Keyword Search
+        26722 => '', // European Candidates PVDA Keyword Search
+        26713 => '', // European Candidates SP.a Keyword Search
+        26718 => '', // European Candidates Vlaams Belang Keyword Search
+// PROJECT Candidates - European - Social Profile
+        25663 => '', // European Candidates CD&V Social Profiles
+        25657 => '', // European Candidates CDH Social Profiles
+        26736 => '', // European Candidates Debout Les Belges Social Profiles
+        25664 => '', // European Candidates Ecolo Social Profiles
+        25658 => '', // European Candidates FDF Social Profiles
+        25656 => '', // European Candidates Groen Social Profiles
+        26759 => '', // European Candidates MR Social Profiles
+        25661 => '', // European Candidates NV-A Social Profiles
+        25660 => '', // European Candidates Open VLD Social Profiles
+        26738 => '', // European Candidates PS Social Profiles
+        25654 => '', // European Candidates PTB Social Profiles
+        26721 => '', // European Candidates PVDA Social Profiles
+        25662 => '', // European Candidates SP.a Social Profiles
+        25659 => '', // European Candidates Vlaams Belang Social Profiles
+// PROJECT Candidates - Federal - Lijsttrekkers Keyword
+        26804 => '', // Federal Candidates B.U.B. FR Lijsttrekkers Keyword
+        26795 => '', // Federal Candidates B.U.B. Lijsttrekkers Keyword
+        26788 => '', // Federal Candidates CD&V Lijsttrekkers Keyword
+        26798 => '', // Federal Candidates cdH. Lijsttrekkers Keyword
+        26799 => '', // Federal Candidates Ecolo Lijsttrekkers Keyword
+        26800 => '', // Federal Candidates FDF Lijsttrekkers Keyword
+        26789 => '', // Federal Candidates GROEN Lijsttrekkers Keyword
+        26805 => '', // Federal Candidates LA DROITE Lijsttrekkers Keyword
+        26790 => '', // Federal Candidates LDD Lijsttrekkers Keyword
+        26801 => '', // Federal Candidates MR Lijsttrekkers Keyword
+        26791 => '', // Federal Candidates N-VA Lijsttrekkers Keyword
+        26792 => '', // Federal Candidates Open VLD Lijsttrekkers Keyword
+        26806 => '', // Federal Candidates Pirate Lijsttrekkers Keyword
+        26797 => '', // Federal Candidates PiratenPartij Lijsttrekkers Keyword
+        26802 => '', // Federal Candidates PS Lijsttrekkers Keyword
+        26803 => '', // Federal Candidates PTB-GO! Lijsttrekkers Keyword
+        26796 => '', // Federal Candidates PVDA Lijsttrekkers Keyword
+        26793 => '', // Federal Candidates sp.a Lijsttrekkers Keyword
+        26794 => '', // Federal Candidates Vlaams Belang Lijsttrekkers Keyword
+// PROJECT Candidates - Federal - Social Profiles
+        25558 => '', // Federal Candidates CD&V Social Profiles
+        25559 => '', // Federal Candidates CDH Social Profiles
+        25560 => '', // Federal Candidates Ecolo Social Profiles
+        25561 => '', // Federal Candidates FDF Social Profiles
+        25570 => '', // Federal Candidates Groen Social Profiles
+        25562 => '', // Federal Candidates MR Social Profiles
+        25563 => '', // Federal Candidates NVA Social Profiles
+        25571 => '', // Federal Candidates Open VLD Social Profiles
+        25565 => '', // Federal Candidates PS Social Profiles
+        25566 => '', // Federal Candidates PTB Social Profiles
+        25567 => '', // Federal Candidates PVDA Social Profiles
+        25568 => '', // Federal Candidates SP.a Social Profiles
+        25569 => '', // Federal Candidates Vlaams Belang Social Profiles
+// PROJECT Candidates - Flemish - Lijsttrekkers Keyword
+        26808 => '', // Flemish Candidates CD&V Lijsttrekkers Keyword
+        26809 => '', // Flemish Candidates Groen Lijsttrekkers Keyword
+        26810 => '', // Flemish Candidates N-VA Lijsttrekkers Keyword
+        26811 => '', // Flemish Candidates Open Vld Lijsttrekkers Keyword
+        26815 => '', // Flemish Candidates PVDA+ Lijsttrekkers Keyword
+        26817 => '', // Flemish Candidates R.O.S.S.E.M. Lijsttrekkers Keyword
+        26812 => '', // Flemish Candidates sp.a Lijsttrekkers Keyword
+        26813 => '', // Flemish Candidates UF Lijsttrekkers Keyword
+        26816 => '', // Flemish Candidates VCP Lijsttrekkers Keyword
+        26814 => '', // Flemish Candidates Vlaams Belang Lijsttrekkers Keyword
+// PROJECT Elections General
+        25544 => '', // Elections Belgium
+        26755 => '', // Elections Europe
+// PROJECT Parties - General Social Profiles
         26758 => 'bub', // B.U.B Social Profiles
         25545 => 'cdenv', // CD&V Social Profiles
         25546 => 'cdh', // CDH Social Profiles
@@ -27,6 +107,6 @@ class Configuration {
         25554 => 'ptb', // PTB Social Profiles
         25555 => 'pvda', // PVDA Social Profiles
         25556 => 'spa', // Sp.a Social Profiles
-        25557 => 'vlaamsbelang' // Vlaams Belang Social Profiles
+        25557 => 'vlaamsbelang' // Vlaams Belang Social Profiles      
     );
 }
